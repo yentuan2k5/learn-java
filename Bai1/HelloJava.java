ea1309b1aaa686eed1e8f3276ad7796c7dc714ad
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-08-28 19:31:48 +0700

    feat: add  HelloWord and Infor program

---
+++ Bai1/HelloJava.java
@@ -0,0 +1,11 @@
+package Bai1;
+
+/**
+ * HelloJava
+ */
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
---
+++ Bai1/Infor.java
@@ -0,0 +1,18 @@
+package Bai1;
+
+/**
+ * Infor
+ */
+public class Infor {
+
+    public static void main(String[] args) {
+        String name = "Tô Nguyễn Tuấn Yên";
+        int age = 21;
+        boolean isStudent = true;
+        boolean isMale = true;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Is Male: " + isMale);
+    }
+}
