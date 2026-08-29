1f4097c8ced22871e383c78d133dd7ba31984080
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-08-29 21:40:46 +0700

    feat: homework DAY02

---
+++ Day02/Bai1.java
@@ -0,0 +1,10 @@
+package Day02;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 25;
+        System.out.println((char) a);
+        System.out.println((char) b);
+    }
+}
---
+++ Day02/Bai2.java
@@ -0,0 +1,13 @@
+package Day02;
+
+/**
+ * Bai2
+ */
+public class Bai2 {
+
+    public static void main(String[] args) {
+        char ch = 'B';
+        int ascii = (int) ch;
+        System.out.println(ascii);
+    }
+}
