a2849e153eb5767252f7792b951434ca18cd4788
To Nguyen Tuan Yen <yentuan2k5@gmail.com>
2026-08-29 21:54:04 +0700

    Fix typo in output message from 'Word' to 'World'

--- Bai1/HelloJava.java
+++ Bai1/HelloJava.java
@@ -6,6 +6,6 @@ package Bai1;
 public class HelloJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        System.out.println("Hello World!");
     }
 }
