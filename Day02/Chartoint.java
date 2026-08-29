c60a55ac4adc36312e21bc755bff114623b7075e
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-08-29 21:31:37 +0700

    feat: day2 asci

---
+++ Day02/Chartoint.java
@@ -0,0 +1,14 @@
+package Day02;
+
+/**
+ * Chartoint
+ */
+public class Chartoint {
+
+    public static void main(String[] args) {
+        char ch = 'A';
+        int ascii = (int) ch;
+        System.out.println(ch);
+        System.out.println(ascii);
+    }
+}
